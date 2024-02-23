8036bdb3958f159a254d006c3897b8e76f055e3a
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-02-23 23:24:27 +0300

    fixed typo

--- users/ahmet/core/nixvim/default.nix
+++ users/ahmet/core/nixvim/default.nix
@@ -38,7 +38,7 @@
         vim.o.foldlevelstart     = 99
         vim.o.foldenable         = true
         vim.o.fillchars          = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-        vim.cmd [[set signcolumn = yes]]
+        vim.cmd [[set signcolumn=yes]]
 
         vim.cmd("set ignorecase")
         vim.cmd("set smartcase")
