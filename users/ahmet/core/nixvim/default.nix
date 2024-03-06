2f61a78b31e74ef91d5aa600d11fbc97aa626d4e
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-03-07 00:41:51 +0300

    sync with upstream name change

--- users/ahmet/core/nixvim/default.nix
+++ users/ahmet/core/nixvim/default.nix
@@ -569,35 +569,7 @@
         };
         notify.enable = true;
 
-        nvim-cmp = {
-          enable = true;
-          mapping = {
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-e>" = "cmp.mapping.close()";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
-            "<S-Tab>" = {
-              action = "cmp.mapping.select_prev_item()";
-              modes = [ "i" "s" ];
-            };
-            "<Tab>" = {
-              action = "cmp.mapping.select_next_item()";
-              modes = [ "i" "s" ];
-            };
-          };
-          snippet.expand = "luasnip";
-          sources = [
-            { name = "nvim_lsp"; }
-            { name = "luasnip"; }
-            { name = "nvim_lua"; }
-            { name = "treesitter"; }
-            { name = "buffer"; }
-            { name = "cmdline"; }
-            { name = "path"; }
-            { name = "async_path"; }
-          ];
-        };
+        cmp.enable = true;
         nvim-ufo.enable = true;
 
         oil = {
