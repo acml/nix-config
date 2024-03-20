1bd36ef9afa3673ff8d517ef9c19083aee971bc1
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2024-03-20 09:25:21 +0300

    removed unused config option 'gist'

--- users/ahmet/core/emacs/doom.d/init.el
+++ users/ahmet/core/emacs/doom.d/init.el
@@ -95,7 +95,6 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)   ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        (lookup +dictionary +docsets +offline) ; navigate your code and its documentation
        (lsp +peek)       ; M-x vscode
        magit             ; a git porcelain for Emacs
