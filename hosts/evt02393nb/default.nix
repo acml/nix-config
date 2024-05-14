82be6d0fc50c2d68b42824311b488f97128cda87
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2024-05-14 19:31:06 +0300

    Temporary fix for WSLg keyboard layout

--- hosts/evt02393nb/default.nix
+++ hosts/evt02393nb/default.nix
@@ -27,6 +27,16 @@
         if [ -f /etc/bashrc ]; then
         . /etc/bashrc
         fi
+
+        # Temporary fix for WSLg keyboard layout (only change the first line)
+        KEYMAP_LAYOUT=us
+        KEYMAP_VARIANT=colemak
+        if [ ! -f /mnt/wsl/state-wslg-config-initialized ]; then
+        read -r -d "" GWSL_SYSTEM_CONFIG_COMMANDS <<EOF
+        KEYMAP_LAYOUT=\`grep -r '^keymap_layout=.*\$' /home/wslg/.config/weston.ini\`;if [ -z \$KEYMAP_LAYOUT ]; then sed -i '$ a\[keyboard]\nkeymap_layout=`echo ''${KEYMAP_LAYOUT}`\nkeymap_variant=`echo ''${KEYMAP_VARIANT}`\n' /home/wslg/.config/weston.ini;pkill -HUP weston;touch /mnt/wsl/state-wslg-config-initialized;fi
+        EOF
+        wsl.exe -d $WSL_DISTRO_NAME --system ''${GWSL_SYSTEM_CONFIG_COMMANDS}
+        fi
       '';
     };
     git.userEmail = lib.mkForce "ahmet.ozgezer@siemens.com";
