5a7a12b4f1cb926ef9dacabcf500eaa2283656f8
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2024-05-23 15:45:55 +0300

    heredoc fix

--- hosts/darkstar/default.nix
+++ hosts/darkstar/default.nix
@@ -35,7 +35,7 @@
         if [ ! -f /mnt/wsl/state-wslg-config-initialized ]; then
           read -r -d "" GWSL_SYSTEM_CONFIG_COMMANDS <<EOF
             KEYMAP_LAYOUT=\`grep -r '^keymap_layout=.*\$' /home/wslg/.config/weston.ini\`;if [ -z \$KEYMAP_LAYOUT ]; then sed -i '$ a\[keyboard]\nkeymap_layout=`echo ''${KEYMAP_LAYOUT}`\nkeymap_variant=`echo ''${KEYMAP_VARIANT}`\n' /home/wslg/.config/weston.ini;pkill -HUP weston;touch /mnt/wsl/state-wslg-config-initialized;fi
-          EOF
+        EOF
           wsl.exe -d $WSL_DISTRO_NAME --system ''${GWSL_SYSTEM_CONFIG_COMMANDS}
         fi
       '';
