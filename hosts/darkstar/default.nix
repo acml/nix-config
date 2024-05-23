81579ff82a54fd85cc80752969ee50e4d9db24c9
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-05-23 15:24:04 +0300

    keyboard localization for GUI

--- hosts/darkstar/default.nix
+++ hosts/darkstar/default.nix
@@ -28,20 +28,22 @@
         if [ -f /etc/bashrc ]; then
         . /etc/bashrc
         fi
-        export PATH="$PATH:$HOME/.toolbox/bin:/apollo/env/bt-rust/bin"
+
+        # Temporary fix for WSLg keyboard layout (only change the first line)
+        KEYMAP_LAYOUT=us
+        KEYMAP_VARIANT=colemak
+        if [ ! -f /mnt/wsl/state-wslg-config-initialized ]; then
+          read -r -d "" GWSL_SYSTEM_CONFIG_COMMANDS <<EOF
+            KEYMAP_LAYOUT=\`grep -r '^keymap_layout=.*\$' /home/wslg/.config/weston.ini\`;if [ -z \$KEYMAP_LAYOUT ]; then sed -i '$ a\[keyboard]\nkeymap_layout=`echo ''${KEYMAP_LAYOUT}`\nkeymap_variant=`echo ''${KEYMAP_VARIANT}`\n' /home/wslg/.config/weston.ini;pkill -HUP weston;touch /mnt/wsl/state-wslg-config-initialized;fi
+          EOF
+          wsl.exe -d $WSL_DISTRO_NAME --system ''${GWSL_SYSTEM_CONFIG_COMMANDS}
+        fi
       '';
     };
-    fish.shellInit = ''
-      fish_add_path --append --path "$HOME/.toolbox/bin"
-      fish_add_path --append --path "/apollo/env/bt-rust/bin"
-    '';
     git.userEmail = lib.mkForce "ozgezer@gmail.com";
     zsh = {
       initExtraBeforeCompInit = ''
         fpath+=("$HOME/.zsh/completion")
-      '';
-      initExtra = ''
-        export PATH="$PATH:$HOME/.toolbox/bin:/apollo/env/bt-rust/bin"
       '';
     };
   };
