6dceb06eaabeffd7044cb6173d8e1099880c6c1a
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2024-05-31 12:17:58 +0300

    sign column hl fix

--- users/ahmet/core/nixvim/default.nix
+++ users/ahmet/core/nixvim/default.nix
@@ -72,7 +72,7 @@
         local signs = { Error = "", Warn = "", Hint = "", Info = "" }
         for type, icon in pairs(signs) do
           local hl = "DiagnosticSign" .. type
-          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         end
 
         vim.api.nvim_create_autocmd('FileType', {
