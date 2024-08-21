dde639de5b6c22aac77c4396c665a52b0c2f34f5
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2024-08-21 16:55:34 +0300

    emacs: workspace tab-bar settings continued

--- users/ahmet/core/emacs/doom.d/config.el
+++ users/ahmet/core/emacs/doom.d/config.el
@@ -602,9 +602,9 @@ the sequences will be lost."
                 for i to (length names)
                 collect
                 (propertize (format " %d %s " (1+ i) name)
-                                    'face (if (equal current-name name)
-                                        '+workspace-tab-selected-face
-                                      '+workspace-tab-face)))
+                            'face (if (equal current-name name)
+                                      '+workspace-tab-selected-face
+                                    '+workspace-tab-face)))
        " ")))
 
   (defun hy/invisible-current-workspace ()
@@ -613,12 +613,15 @@ current workspace), so we invisibly print the current workspace
 name as well to trigger updates"
     (propertize (safe-persp-name (get-current-persp)) 'invisible t))
 
-  (customize-set-variable 'tab-bar-format '(workspaces-formatted tab-bar-format-align-right hy/invisible-current-workspace))
+  (customize-set-variable 'tab-bar-format '(workspaces-formatted
+                                            tab-bar-format-align-right
+                                            hy/invisible-current-workspace))
 
   ;; don't show current workspaces when we switch, since we always see them
-  (advice-add #'+workspace/display :override #'ignore)
+  ;; (advice-add #'+workspace/display :override #'ignore)
   ;; same for renaming and deleting (and saving, but oh well)
-  (advice-add #'+workspace-message :override #'ignore))
+  ;; (advice-add #'+workspace-message :override #'ignore)
+  )
 
 ;; need to run this later for it to not break frame size for some reason
 (run-at-time nil nil (cmd! (tab-bar-mode +1)))
