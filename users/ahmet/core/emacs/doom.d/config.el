66434191a0ad8cb1d961a752f353d70d55314c3a
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-08-21 08:48:18 +0300

    persp-mode tab-bar integration

--- users/ahmet/core/emacs/doom.d/config.el
+++ users/ahmet/core/emacs/doom.d/config.el
@@ -588,14 +588,46 @@ the sequences will be lost."
 (use-package! pdf-tools :defer t
   :hook ((pdf-view-mode . pdf-view-themed-minor-mode)))
 
+(custom-set-faces!
+  '(+workspace-tab-selected-face :inherit tab-bar-tab :height 135)
+  '(+workspace-tab-face :inherit tab-bar-tab-inactive))
+
 (after! persp-mode
-  (setq persp-emacsclient-init-frame-behaviour-override nil)
-  (defun display-workspaces-in-minibuffer ()
-    (with-current-buffer " *Minibuf-0*"
-      (erase-buffer)
-      (insert (+workspace--tabline))))
-  (run-with-idle-timer 1 t #'display-workspaces-in-minibuffer)
-  (+workspace/display))
+  (defun workspaces-formatted ()
+    (let ((names (or (seq-filter (lambda (n) (not (string= n "none"))) persp-names-cache) nil))
+          (current-name (safe-persp-name (get-current-persp))))
+      (mapconcat
+       #'identity
+       (cl-loop for name in names
+                for i to (length names)
+                collect
+                (concat (propertize (format " %d" (1+ i)) 'face
+                                    `(:inherit ,(if (equal current-name name)
+                                                    '+workspace-tab-selected-face
+                                                  '+workspace-tab-face)
+                                      :weight bold))
+                        (propertize (format " %s " name) 'face
+                                    (if (equal current-name name)
+                                        '+workspace-tab-selected-face
+                                      '+workspace-tab-face))))
+       " ")))
+
+  (defun hy/invisible-current-workspace ()
+    "The tab bar doesn't update when only faces change (i.e. the
+current workspace), so we invisibly print the current workspace
+name as well to trigger updates"
+    (propertize (safe-persp-name (get-current-persp)) 'invisible t))
+
+  (customize-set-variable 'tab-bar-format '(workspaces-formatted tab-bar-format-align-right hy/invisible-current-workspace))
+
+  ;; don't show current workspaces when we switch, since we always see them
+  (advice-add #'+workspace/display :override #'ignore)
+  ;; same for renaming and deleting (and saving, but oh well)
+  (advice-add #'+workspace-message :override #'ignore))
+
+;; need to run this later for it to not break frame size for some reason
+(run-at-time nil nil (cmd! (tab-bar-mode +1)))
+
 
 (setq +workspaces-switch-project-function #'dired)
 
