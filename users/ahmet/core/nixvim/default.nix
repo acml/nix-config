689291c26ce2e60dcfe00628ee4b304b03ebe562
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-08-30 13:56:57 +0300

    neovim: nvim-cmp settings

--- users/ahmet/core/nixvim/default.nix
+++ users/ahmet/core/nixvim/default.nix
@@ -437,8 +437,73 @@
               # }
             ];
         };
-        clangd-extensions.enable = true;
-        cmp.enable = true;
+        cmp = {
+          enable = true;
+          settings = {
+            snippet = {
+              expand = ''
+                function(args)
+                  require('luasnip').lsp_expand(args.body)
+                end
+              '';
+            };
+            completion = {
+              completeopt = "menu,menuone,noinsert";
+            };
+            mapping = {
+              # Scroll the documentation window [b]ack / [f]orward
+              "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+
+              "<CR>" = "cmp.mapping.confirm { select = true }";
+
+              "<C-n>" = "cmp.mapping.select_next_item()";
+              "<Tab>" = "cmp.mapping.select_next_item()";
+
+              "<C-p>" = "cmp.mapping.select_prev_item()";
+              "<S-Tab>" = "cmp.mapping.select_prev_item()";
+
+              # Manually trigger a completion from nvim-cmp.
+              #  Generally you don't need this, because nvim-cmp will display
+              #  completions whenever it has completion options available.
+              # "<C-Space>" = "cmp.mapping.complete {}";
+
+              # Think of <c-l> as moving to the right of your snippet expansion.
+              #  So if you have a snippet that's like:
+              #  function $name($args)
+              #    $body
+              #  end
+              #
+              # <c-l> will move you to the right of the expansion locations.
+              # <c-h> is similar, except moving you backwards.
+              "<C-l>" = ''
+                cmp.mapping(function()
+                  if luasnip.expand_or_locally_jumpable() then
+                    luasnip.expand_or_jump()
+                  end
+                end, { 'i', 's' })
+              '';
+              "<C-h>" = ''
+                cmp.mapping(function()
+                  if luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                  end
+                end, { 'i', 's' })
+              '';
+              # For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+              #    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+            };
+            # WARNING: If plugins.cmp.autoEnableSources Nixivm will automatically enable the
+            # corresponding source plugins. This will work only when this option is set to a list.
+            # If you use a raw lua string, you will need to explicitly enable the relevant source
+            # plugins in your nixvim configuration.
+            sources = [
+              { name = "luasnip"; }
+              { name = "nvim_lsp"; }
+              { name = "path"; }
+            ];
+          };
+        };
         dap.enable = true;
         debugprint.enable = true;
         diffview.enable = true;
@@ -536,7 +601,6 @@
           globalstatus = true;
           ignoreFocus = [ "neo-tree" ];
         };
-        luasnip.enable = true;
         mini = {
           enable = true;
           modules = {
