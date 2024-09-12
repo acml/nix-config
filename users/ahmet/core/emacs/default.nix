ea1e6cb6930ac35e0248f39caf9848e04edc630e
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-09-12 20:16:21 +0300

    emacs: applied patches for darwin build

--- users/ahmet/core/emacs/default.nix
+++ users/ahmet/core/emacs/default.nix
@@ -189,7 +189,37 @@ lib.mkMerge [
         enable = true;
         package = lib.mkMerge [
           (lib.mkIf isLinux pkgs.emacs29)
-          (lib.mkIf isDarwin pkgs.emacs29)
+          (lib.mkIf isDarwin (pkgs.emacs29-pgtk.overrideAttrs (old: {
+            patches =
+              (old.patches or [ ])
+              ++ [
+                # Fix OS window role (needed for window managers like yabai)
+                (pkgs.fetchpatch {
+                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+                  sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+                })
+                # no-frame-refocus-cocoa
+                (pkgs.fetchpatch {
+                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/no-frame-refocus-cocoa.patch";
+                  sha256 = "sha256-QLGplGoRpM4qgrIAJIbVJJsa4xj34axwT3LiWt++j/c=";
+                })
+                # Use poll instead of select to get file descriptors
+                (pkgs.fetchpatch {
+                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
+                  sha256 = "sha256-jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
+                })
+                # Enable rounded window with no decoration
+                (pkgs.fetchpatch {
+                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+                  sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+                })
+                # Make Emacs aware of OS-level light/dark mode
+                (pkgs.fetchpatch {
+                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+                  sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
+                })
+              ];
+          })))
         ];
         extraPackages = epkgs: (with epkgs; [
           djvu
