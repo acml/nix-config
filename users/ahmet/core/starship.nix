53385c03badf53dfa9d21da2f3d9082cbb439206
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2024-10-05 21:44:33 +0300

    starship: theme change

--- users/ahmet/core/starship.nix
+++ users/ahmet/core/starship.nix
@@ -1,12 +1,6 @@
 { lib, ... }: {
   programs.starship = {
     enable = true;
-    settings = {
-      add_newline = false;
-      character = {
-        success_symbol = "[➤](bold green)";
-        error_symbol = "[➤](bold red)";
-      };
-    };
   };
+  xdg.configFile."starship.toml".source = lib.mkForce ./starship.toml;
 }
