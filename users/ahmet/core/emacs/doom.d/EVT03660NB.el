3faff31bf3b15925cd2bdee72a6b2bd0981a41a7
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2025-07-04 00:13:44 +0300

    emacs: register new project types

--- users/ahmet/core/emacs/doom.d/EVT03660NB.el
+++ users/ahmet/core/emacs/doom.d/EVT03660NB.el
@@ -21,17 +21,43 @@
 
 
 (after! projectile
-  (defun acml/compilation-dir ()
-    "CMake compile command."
-    (format "%s/csd" (string-trim (cdr (assoc 'mainFolders (parse-file-content (expand-file-name "proj.default.ini" (projectile-project-root))))) "\"" "\"")))
+
+  (defun acml/projectile-cm12425-project-p (&optional dir)
+    "Check if a project contains cm12425 files.
+When DIR is specified it checks DIR's project, otherwise
+it acts on the current project."
+    (and (projectile-verify-files '("cp1200" "le_nbg2") dir)
+         (not (projectile-verify-file "tools" dir))))
+
+  (projectile-register-project-type 'cm12425 #'acml/projectile-cm12425-project-p
+                                    :compilation-dir "cp1200/cp1242-5/make"
+                                    :compile "build_cp_1242-5.bat")
+
+  (defun acml/projectile-cm12435-project-p (&optional dir)
+    "Check if a project contains cm12435 files.
+When DIR is specified it checks DIR's project, otherwise
+it acts on the current project."
+    (and (projectile-verify-files '("audis_tools" "cp1200" "le_nbg" "le_nbg2" "tools") dir)
+         (not (projectile-verify-file "cp1500" dir))))
+
+  (projectile-register-project-type 'cm12435 #'acml/projectile-cm12435-project-p
+                                    :compilation-dir "cp1200/cp1243-5/csd"
+                                    :compile "make -j$(nproc) -s all_targets 2>&1")
+
   (projectile-register-project-type 'cp1200 '("audis_linux" "audis_tools" "audis_utils" "cp1200" "cp1500" "le_nbg2")
                                     :compilation-dir "cp1200/cp1243-1/csd"
                                     :compile "script --quiet --return --log-out build-$(date -Iseconds).log --command \"make -j$(nproc) -s all_targets 2>&1\"")
+
+  (defun acml/compilation-dir ()
+    "Project compile command."
+    (format "%s/csd" (string-trim (cdr (assoc 'mainFolders (parse-file-content (expand-file-name "proj.default.ini" (projectile-project-root))))) "\"" "\"")))
+
   (projectile-register-project-type 'cp1200dt '("proj.default.ini")
                                     :project-file "proj.default.ini"
                                     :compilation-dir #'acml/compilation-dir
                                     :compile "script --quiet --return --log-out build-$(date -Iseconds).log --command \"./docker_make.sh -j$(nproc) -s all_targets 2>&1\""
-                                    :configure "/usr/bin/git dt checkout -f"))
+                                    :configure "/usr/bin/git dt checkout -f")
+  )
 
 (add-to-list 'auto-mode-alist '("\\.igt" . makefile-mode))
 
