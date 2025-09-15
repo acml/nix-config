5b47805797dc50c35b1671de68c4e3b7e083ff3c
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2025-09-15 22:30:58 +0300

    emacs: eglot setting is not working. Commented out!

--- users/ahmet/core/emacs/doom.d/config.el
+++ users/ahmet/core/emacs/doom.d/config.el
@@ -281,7 +281,8 @@
 
 (after! eglot
   (set-eglot-client! '(c-mode c-ts-mode c++-mode c++-ts-mode objc-mode) `("ccls" ,(concat "--init={\"cache\": {\"directory\": \"" (file-truename "~/.cache/ccls") "\"}}")))
-  (set-eglot-client! '(nix-mode) '("nil" :initializationOptions (:nil (:nix (:flake (:autoArchive t)))))))
+  ;; (set-eglot-client! 'nix-mode '("nil" "--stdio" :initializationOptions (:nil (:nix (:flake (:autoArchive t))))))
+  )
 
 ;; Easier to match with a bspwm rule:
 ;;   bspc rule -a 'Emacs:emacs-everywhere' state=floating sticky=on
