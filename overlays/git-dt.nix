44eccfd3ee5d51560e573e07c577a4619024ff7d
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2026-02-03 08:54:21 +0300

    chore(git-dt): Bump to 4.2.5-1-1
    Update fetchurl sha256 to match the new deb package version

--- overlays/git-dt.nix
+++ overlays/git-dt.nix
@@ -13,11 +13,11 @@ final: _: {
     }:
     stdenv.mkDerivation rec {
       name = "git-dt";
-      version = "4.2.4-0-1";
+      version = "4.2.5-1-1";
 
       src = fetchurl {
         url = "https://captain.rtf.siemens.net/artifactory/simaticnet_wss_gitdt-stable-debian-egll/pool/git-dt_${version}_all.deb";
-        hash = "sha256-GAPOdXiCS6IXi3g2CxyeXVt7Yczmsla6mFO5m3bc8XI=";
+        hash = "sha256-eFqsqF8q4u1c8ev1TXczcmXr9IjBRGnJe1A79sNucQE=";
       };
 
       buildInputs = [
