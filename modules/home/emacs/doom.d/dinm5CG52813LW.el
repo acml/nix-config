d022c68d7eaece954841cd13fd9ad84bab991a7f
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2026-06-06 15:53:17 +0300

    chore(emacs): Comment out gptel-magit model configuration

--- modules/home/emacs/doom.d/dinm5CG52813LW.el
+++ modules/home/emacs/doom.d/dinm5CG52813LW.el
@@ -222,8 +222,9 @@ ORIG-FUN is the original function, FILENAME is the file being processed, ARGS ar
 (use-package! gptel-magit
   :after gptel magit
   :config
-  (setq gptel-magit-model 'gpt-5.2
-        gptel-magit-backend (gptel-make-gh-copilot "Copilot")))
+  ;; (setq gptel-magit-model 'gpt-5.5
+  ;;       gptel-magit-backend (gptel-make-gh-copilot "Copilot"))
+  )
 
 (after! dirvish
   (setq dirvish-quick-access-entries
