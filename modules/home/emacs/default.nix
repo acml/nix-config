475ddb09817c48e85a0b0dd318b0b07d67f6d27e
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2026-06-06 16:55:36 +0300

    fix(emacs): Build ghostel from source with custom derivation
    Replace the nixpkgs ghostel package with a manually built
    derivation using zig, allowing pinning to a specific commit
    and providing better control over build flags.

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -52,7 +52,7 @@ let
       copilot
       djvu
       emacsql
-      ghostel
+      # ghostel
       tree-sitter-langs
       treesit-grammars.with-all-grammars
       # (treesit-grammars.with-grammars (
@@ -91,6 +91,65 @@ let
       #   ]
       # ))
       vterm
+    ]
+    ++ [
+      (
+        let
+          zig = pkgs.zig_0_15;
+          pname = "ghostel";
+          version = "20260606";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "dakra";
+            repo = "ghostel";
+            rev = "a614f0f30d8a65da928a840e62cc9b2c6a1381af";
+            hash = "sha256-vG7JfgXUm6b1zf0WgyDlvy29wpTvcI/X74NP8s66c/4=";
+          };
+
+          module = pkgs.stdenv.mkDerivation (finalAttrs: {
+            inherit pname version src;
+
+            deps = zig.fetchDeps {
+              inherit (finalAttrs) src pname version;
+              fetchAll = true;
+              hash = "sha256-CTsG3dXu3DECDbklBAtr2fYou82WNvQ1Q3JET0TmuyM=";
+            };
+            nativeBuildInputs = [ zig ];
+            env.EMACS_INCLUDE_DIR = "${emacs}/include";
+            dontSetZigDefaultFlags = true;
+            doCheck = true;
+            zigCheckFlags = [
+              "-Dcpu=baseline"
+              # See https://github.com/ghostty-org/ghostty/blob/main/PACKAGING.md#build-options
+              "-Doptimize=ReleaseFast"
+            ];
+            zigBuildFlags = finalAttrs.zigCheckFlags;
+
+            postConfigure = ''
+              cp -rLT ${finalAttrs.deps} "$ZIG_GLOBAL_CACHE_DIR/p"
+              chmod -R u+w "$ZIG_GLOBAL_CACHE_DIR/p"
+            '';
+          });
+          libExt = pkgs.stdenv.hostPlatform.extensions.sharedLibrary;
+        in
+        melpaBuild {
+          inherit pname version src;
+
+          files = ''
+            (:defaults "etc" "ghostel-module${libExt}")
+          '';
+
+          preBuild = ''
+            install ${module}/lib/libghostel-module${libExt} ghostel-module${libExt}
+          '';
+
+          passthru = {
+            updateScript = nix-update-script { extraArgs = [ "--version=branch=main" ]; };
+
+            inherit module;
+          };
+        }
+      )
     ]
     ++ lib.optionals isDarwin [
       pdf-tools
